d490b5412c1bf0da1fd7536ef4bc624fd6dbc797
aadhithya04072006-cyber <aadhithya04072006@gmail.com>
2026-08-17 10:32:03 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First GitHub Project</title>
+</head>
+<body>
+
+    <h1>Hello, GitHub! 👋</h1>
+    <p>This is my first project uploaded to GitHub.</p>
+
+    <button onclick="showMessage()">Click Me</button>
+
+    <script>
+        function showMessage() {
+            alert("Hello! My code is working 🎉");
+        }
+    </script>
+
+</body>
+</html>
